5cf4e2bce43136936101a285bfc3ccc92276cdb9
[mellostack] <[fluiz7880@gmail.com]>
2024-01-28 11:17:34 -0300

    CHATAPP DB CONFIGURATE

--- chatAPP/client-side/my-app/app/page.tsx
+++ chatAPP/client-side/my-app/app/page.tsx
@@ -1,20 +1,22 @@
-'use client'
+"use client";
 
 // import { useCartStore } from "./modules/zus";
 import { Button } from "@/components/ui/button";
+import Login from "./modules/login";
+import { useState } from "react";
 
 export default function Home() {
-
+  const [isLogged, setisLogged] = useState(false);
+  
   // const {currentId, addCurrentId: changeID} = useCartStore()
-
 
   // const test = () => {
   //     changeID(5)
   // }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h3></h3>
-    </main>
+    <>
+      {!isLogged ? <Login /> : "Chat"}
+    </>
   );
 }
--- chatAPP/server-side/server.js
+++ chatAPP/server-side/server.js
@@ -1,16 +1,63 @@
-const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config()
+const { createClient } = require("@supabase/supabase-js");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
 
-const supabaseUrl = 'https://apkuarlppngqawvovkqh.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = "https://apkuarlppngqawvovkqh.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // const { data, error } = await supabase
 // .from('Users')
 // .select()
 
-app.listen(port, () => console.log("Ouvindo na Porta: "+port))
+app.use(bodyParser.json());
+
+async function fetchUsers() {
+  const { data, error } = await supabase.from("Users").select();
+
+  return data;
+}
+
+async function fetchMsg() {
+  const { data, error } = await supabase.from("Msg").select();
+
+  return data;
+}
+
+const fetchedUsers = fetchUsers();
+const fetchedMsg = fetchMsg();
+
+app.get("/api/users", (req, res) => {
+  fetchedUsers.then((x) => {
+    const names = x.map((obj) => obj.name);
+
+    res.json(names);
+  });
+});
+
+app.post("/api/login", (req, res) => {
+  //   data.then((data) => {
+  //     if (data[0].email === req.body[0].email) {
+  //         res.json([{id: data[0].id}])
+  //     }
+  //     else{
+  //         res.json("Wrong Email")
+  //     }
+  //   });
+});
+
+app.get("/api/messages", (req, res) => {
+  fetchedMsg.then((x) => {
+    const Msg = x.map((obj) => obj);
+
+    res.json(Msg);
+  });
+});
+
+app.post("/api/mensage/add", (req, res) => {});
+
+app.listen(port, () => console.log("Ouvindo na Porta: " + port));
